89e3a55dfbbd1c12c33599966d3223c0506e1653
andre <apracapinheiro@yahoo.com.br>
2017-06-05 08:57:00 -0300

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+
+from .models import Post
+
+
+def post_list(request):
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)  # 3 posts em cada página
+    page = request.GET.get('page')
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # Se a página não é um inteiro, mostra a primeira página
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Se a página está fora do alcance da última página de resultados
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month,
+                             publish__day=day)
+    return render(request, 'blog/post/detail.html', {'post': post})
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
