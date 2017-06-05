ece225c1fc6b1846b9f31b2fc4c3cc86d492d911
andre <apracapinheiro@yahoo.com.br>
2017-06-05 17:32:42 -0300

    - capitulo 3 - sitemaps e feed

---
+++ blog/feeds.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from django.contrib.syndication.views import Feed
+from django.template.defaultfilters import truncatewords
+from .models import Post
+
+
+class LatestPostsFeed(Feed):
+    title = 'Meu Blog'
+    link = '/blog'
+    description = 'Novos posts do meu blog'
+
+    def items(self):
+        return Post.published.all()[:5]
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return truncatewords(item.body, 30)
