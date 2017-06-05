7a4de68a648f537cc774c073181c25d1d98e459f
andre <apracapinheiro@yahoo.com.br>
2017-06-05 14:52:39 -0300

    - Continuando com o tutorial, fim do capitulo 2.

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
+from django.db.models import Count
 
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
@@ -51,7 +52,13 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
-    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'comment_form': comment_form})
+    # Lista de posts similares de acordo com as tags do post atual
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'comment_form': comment_form,
+                                                     'similar_posts': similar_posts})
 
 
 class PostListView(ListView):
